88b0779e1832fdf4a81d6728ee3ea2a867720443
Evan Geurts <GeurEvan189@GoTVDSB.ca>
2021-03-03 12:53:33 -0500

    Half way

--- Part 5 – Decisions/Program.cs
+++ Part 5 – Decisions/Program.cs
@@ -10,6 +10,42 @@ namespace Part_5___Decisions
     {
         static void Main(string[] args)
         {
+
+            string answer;
+            int age;
+
+
+            Console.WriteLine("Please enter your age.");
+            age = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (age >= 18)
+
+                Console.WriteLine("You are an adult.");
+
+
+            else if (age < 18)
+
+                Console.WriteLine("you are a Teen.");
+
+
+            else if (age <= 5)
+                Console.WriteLine("You are a Toddler");
+
+            else if (age <= 10)
+
+                Console.WriteLine("you are a Child");
+
+            else if (age > 5)
+
+                Console.WriteLine("you are a preteen");
+
+
+
+
+            Console.ReadLine();
+
         }
     }
 }
